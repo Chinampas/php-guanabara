434f010b647b9d895e3cc598315b03791c5047bf
Jose Gabriel <43712088+Chinampas@users.noreply.github.com>
2022-03-17 20:46:53 -0300

    Add files via upload

---
+++ exercicio 4/php4.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP GUANABARA</title>
+</head>
+<body>
+<div>
+    <?php
+        $a = isset($_GET["ano"])?$_GET["ano"]:1900;
+        $i = date("Y") - $a;
+        echo "voce nasceu em $a e tem $i anos"
+    ?>
+</div>
+</body>
+</html>
